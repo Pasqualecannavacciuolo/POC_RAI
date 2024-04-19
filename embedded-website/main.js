d08212fbd40be3a09239250a635b058d6e487aba
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-04-19 13:54:16 +0200

    ignore

--- embedded-website/main.js
+++ embedded-website/main.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 
 app.use((req, res, next) => {
-    const allowedDomains = ["http://127.0.0.1:5500", "https://main-website-gray.vercel.app/"]; // Lista di indirizzi consentiti
+    const allowedDomains = ["https://main-website-gray.vercel.app"]; // Lista di indirizzi consentiti
     const allowedDomainsString = allowedDomains.join(' ');
     res.setHeader('Content-Security-Policy', `frame-ancestors ${allowedDomainsString}`);
     next();
@@ -17,11 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/',  (req, res) => {
     res.sendFile(path.join(__dirname, "public", 'index.html')); // Invia il file HTML come risposta
 });
-
-// Rotta per il contenuto da incorporare in iframe
-/*app.get('/contenuto',  (req, res) => {
-    res.sendFile(path.join(__dirname, "public", 'index.html')); // Invia il file HTML come risposta
-});*/
 
 app.listen(PORT, () => {
     console.log(`Server A in esecuzione su http://localhost:${PORT}`);
